94ca68f93e291ca04e20e76abe477b4fc426da9c
Diego Fernández <diego.j.fernandez.g@gmail.com>
2024-09-18 15:10:49 -0300

    se descubrio y arreglo un error al ingresar un pokemon inexistente mediante un control de flujo

--- parte_2.sh
+++ parte_2.sh
@@ -14,21 +14,26 @@ fi
 for pokemon in ${lista_nombres_pokemon[@]}; do
     #0: (id), 1: (identifier), 2: (altura[metros]), 3: (peso[gramos])
     lista_informacion_pokemon=($(cat $directorio_pokemon_csv | cut -d "," -f 1,2,4,5 | grep ",$pokemon," | tr "," " "))
-    
-    ability_id=($(cat $directorio_pokemon_abilities_csv | cut -d "," -f 1,2 | grep ""^${lista_informacion_pokemon[0]}"," | cut -d "," -f 2)) 
-    altura=$(( ${lista_informacion_pokemon[2]} * 10 ))
-    peso=$(( ${lista_informacion_pokemon[3]} / 10 ))
+    if [[ ${#lista_informacion_pokemon[@]} -lt 1 ]]; then
+        echo -e "$pokemon no existe.\n"
+    elif [[ $pokemon =~ ^[0-9]+$ ]]; then
+        echo -e "Ingresaste un numero entero no un <pokemon>.\n"
+    else
+        ability_id=($(cat $directorio_pokemon_abilities_csv | cut -d "," -f 1,2 | grep ""^${lista_informacion_pokemon[0]}"," | cut -d "," -f 2)) 
+        altura=$(( ${lista_informacion_pokemon[2]} * 10 ))
+        peso=$(( ${lista_informacion_pokemon[3]} / 10 ))
 
-    #imprimir datos del pokemon
-    echo "------------------"
-    echo "Pokemon: $pokemon"
-    echo "Altura: $altura centimetros"
-    echo "Peso: $peso kilos"
-    echo -e "\n Habilidades:"
-    for habilidad in ${ability_id[@]}; do
-        nombre_habilidad=($(cat $directorio_ability_names_csv | cut -d "," -f 1,2,3 | grep ""$habilidad","$idioma"" | cut -d "," -f 3))
-        echo " * $nombre_habilidad"
-    done
-    echo -e "------------------\n"
+        #imprimir datos del pokemon
+        echo "------------------"
+        echo "Pokemon: $pokemon"
+        echo "Altura: $altura centimetros"
+        echo "Peso: $peso kilos"
+        echo -e "\n Habilidades:"
+        for habilidad in ${ability_id[@]}; do
+            nombre_habilidad=($(cat $directorio_ability_names_csv | cut -d "," -f 1,2,3 | grep ""$habilidad","$idioma"" | cut -d "," -f 3))
+            echo " * $nombre_habilidad"
+        done
+        echo -e "------------------\n"
+    fi
 done
 
